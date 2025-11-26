0b901f44ad34df41a7ff92d1fe889c6ec80b35e0
Connor McGurran <cmcgurran11@gmail.com>
2025-11-25 20:24:51 -0500

    Testing the API using Insomnia

--- workspace/littlelemon/LittleLemonAPI/urls.py
+++ workspace/littlelemon/LittleLemonAPI/urls.py
@@ -6,7 +6,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('api-token-auth/', obtain_auth_token),
     path('menu-items/', MenuItemsView.as_view(), name='menu-items'),
-    path('menu-items/<int:pk>/', SingleMenuItemView.as_view(), name='single-menu-item'),
+    path('menu-items/<int:pk>', SingleMenuItemView.as_view(), name='single-menu-item'),
     path('message/', views.msg),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.authtoken'))
